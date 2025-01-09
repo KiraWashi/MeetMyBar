a5800d63148391e4cd53a71c5c29cd86e08c85ce
charlottemenou <charlottemenou01@gmail.com>
2025-01-09 17:02:43 +0100

    Création de la page d'acceuil

---
+++ frontend/app/src/main/java/com/example/frontend/HomeViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.frontend
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel: ViewModel() {
+}
--- frontend/app/src/main/java/com/example/frontend/MainActivity.kt
+++ frontend/app/src/main/java/com/example/frontend/MainActivity.kt
@@ -6,22 +6,62 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.frontend.ui.theme.FrontendTheme
+import com.example.frontend.ui.theme.SpritzClairColor
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             FrontendTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        CenterAlignedTopAppBar(
+                            colors = TopAppBarDefaults.topAppBarColors(
+                                containerColor = SpritzClairColor,
+                                titleContentColor = Color.Black,
+                            ),
+                            title = {
+                                Text("MeetMyBar")
+                            },
+                            navigationIcon = {
+                                IconButton(onClick = { /* do something */ }) {
+                                    Icon(
+                                        imageVector = Icons.Filled.Settings,
+                                        contentDescription = "Settings"
+                                    )
+                                }
+                            },
+                            actions = {
+                                IconButton(onClick = { /* do something */ }) {
+                                    Icon(
+                                        imageVector = Icons.Filled.AccountCircle,
+                                        contentDescription = "Account",
+                                        tint = Color.Black,
+                                    )
+                                }
+                            },
+                        )
+                    },
+                ) { innerPadding ->
+                    HomeScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
