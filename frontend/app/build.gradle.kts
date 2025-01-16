b9d98469dae4748713f97dfdfc33e66571d02493
Kuonagou <anouk.gouhier@gmail.com>
2025-01-16 16:16:13 +0100

    Première version test de la recup de photo sur telephone

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -43,6 +43,10 @@ android {
 
 dependencies {
 
+
+    implementation("io.coil-kt:coil-compose:2.4.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -55,6 +59,7 @@ dependencies {
     implementation(libs.androidx.recyclerview)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.games.activity)
+    implementation(libs.firebase.appdistribution.gradle)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
