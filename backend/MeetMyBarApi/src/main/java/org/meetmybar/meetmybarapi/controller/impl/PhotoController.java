915730e74d509068b32e7cbf3c931e8a0b207028
leo_paugam <thepoubelle29@gmail.com>
2025-01-17 14:45:41 +0100

    PUT DELETE à tester

--- backend/MeetMyBarApi/src/main/java/org/meetmybar/meetmybarapi/controller/impl/PhotoController.java
+++ backend/MeetMyBarApi/src/main/java/org/meetmybar/meetmybarapi/controller/impl/PhotoController.java
@@ -4,13 +4,12 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 
+import jakarta.validation.Valid;
 import org.meetmybar.meetmybarapi.business.impl.PhotoBusiness;
 import org.meetmybar.meetmybarapi.models.dto.Photo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
@@ -53,18 +52,17 @@ public class PhotoController {
     }
 
 
-    //@PatchMapping("/{id}")
-    //@Operation(summary = "Mettre à jour une photo")
-    //public ResponseEntity<Photo> updatePhoto(
-    //       @PathVariable int id,
-    //        @Valid @RequestBody Photo updateDto) {
-    //   return ResponseEntity.ok(photoBusiness.updatePhoto(id, updateDto));
-    // }
+    @PatchMapping("/{id}")
+    @Operation(summary = "Mettre à jour une photo")
+    public ResponseEntity<Photo> updatePhoto(
+           @PathVariable int id,
+            @Valid @RequestBody Photo updateDto) {
+       return ResponseEntity.ok(photoBusiness.updatePhoto(id, updateDto));
+    }
 
-    //@DeleteMapping("/{id}")
-    //@Operation(summary = "Supprimer une photo")
-    //public ResponseEntity<Void> deletePhoto(@PathVariable int id) {
-    //     photoBusiness.deletePhoto(id);
-    //   return ResponseEntity.noContent().build();
-    //}
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Supprimer une photo")
+    public ResponseEntity<Photo> deletePhoto(@PathVariable int id) {
+       return ResponseEntity.ok(photoBusiness.deletePhoto(id));
+    }
 }
