dc4f61260bfaa650f9dfb38c89feb2d1a380fb65
Nathanael.Monnier <nathanael.monnier@arkea.com>
2025-01-31 16:22:42 +0100

    Ajout Tests (A continuer)

--- backend/MeetMyBarApi/src/test/java/org/meetmybar/meetmybarapi/repository/DrinkRepositoryTest.java
+++ backend/MeetMyBarApi/src/test/java/org/meetmybar/meetmybarapi/repository/DrinkRepositoryTest.java
@@ -12,6 +12,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
@@ -21,6 +23,32 @@ public class DrinkRepositoryTest {
 
     @Autowired
     private DrinkRepository drinkRepository;
+
+
+    @Test
+    void testGetDrinks() {
+
+        // Assert
+        this.drinkRepository.createDrink(getDrink());
+        this.drinkRepository.createDrink(getDrinkV2());
+
+        // Act
+        List<Drink> drinks = this.drinkRepository.getDrinks();
+
+        // Assert
+        assertNotNull(drinks.getFirst());
+        assertNotNull(drinks.getLast());
+        Assertions.assertEquals("Lancelot", drinks.getFirst().getBrand());
+        Assertions.assertEquals(1, drinks.getFirst().getId());
+        Assertions.assertEquals("Duchesse", drinks.getFirst().getName());
+        Assertions.assertEquals(7.5, drinks.getFirst().getAlcoholDegree());
+        Assertions.assertEquals(Drink.TypeEnum.BIERE_BLONDE, drinks.getFirst().getType());
+
+        Assertions.assertEquals(4, drinks.getLast().getId());
+        Assertions.assertEquals("Chouffe", drinks.getLast().getName());
+        Assertions.assertEquals(8, drinks.getLast().getAlcoholDegree());
+        Assertions.assertEquals(Drink.TypeEnum.BIERE_ROUGE, drinks.getLast().getType());
+    }
 
     @Test
     void testGetDrinkById() {
@@ -39,13 +67,38 @@ public class DrinkRepositoryTest {
         Assertions.assertEquals(Drink.TypeEnum.BIERE_BLONDE, drink.getType());
     }
 
+    @Test
+    void testGetDrinkByName() {
+        // Arrange
+        this.drinkRepository.createDrink(getDrink());
+
+        // Act
+        Drink drink = this.drinkRepository.getDrinkByName("Duchesse");
+
+        // Assert
+        assertNotNull(drink);
+        Assertions.assertEquals("Lancelot", drink.getBrand());
+        Assertions.assertEquals(1, drink.getId());
+        Assertions.assertEquals("Duchesse", drink.getName());
+        Assertions.assertEquals(7.5, drink.getAlcoholDegree());
+        Assertions.assertEquals(Drink.TypeEnum.BIERE_BLONDE, drink.getType());
+    }
+
     Drink getDrink(){
         Drink drink = new Drink();
         drink.setBrand("Lancelot");
-        drink.setId(1);
         drink.setName("Duchesse");
         drink.setAlcoholDegree(7.5);
         drink.setType(Drink.TypeEnum.BIERE_BLONDE);
+        return drink;
+    }
+
+    Drink getDrinkV2(){
+        Drink drink = new Drink();
+        drink.setBrand("");
+        drink.setName("Chouffe");
+        drink.setAlcoholDegree(8);
+        drink.setType(Drink.TypeEnum.BIERE_ROUGE);
         return drink;
     }
 }
